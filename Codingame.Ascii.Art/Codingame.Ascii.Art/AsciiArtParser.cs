e05529e715343daef13d9812c7e20d933fc7a7b2
alinddicu <alinddicu@yahoo.com>
2016-03-09 20:54:27 +0100

    init

---
+++ Codingame.Ascii.Art/Codingame.Ascii.Art/AsciiArtParser.cs
@@ -0,0 +1,12 @@
+﻿namespace Codingame.Ascii.Art
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+
+    public class AsciiArtParser
+    {
+    }
+}
